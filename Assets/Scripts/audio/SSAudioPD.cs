08777fb377cb7d3e80f8559d6f0809c4a069edad
arduinepo <patrice.andrieu6@gmail.com>
2021-04-28 17:08:13 +0200

    first vertical horizontal angles encodings, ternary discrete modes

--- Assets/Scripts/audio/SSAudioPD.cs
+++ Assets/Scripts/audio/SSAudioPD.cs
@@ -1,27 +1,28 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace audio
 {
     public class SSAudioPD : MonoBehaviour
     {
-        static private float linearLink=-200/Mathf.PI;
-        static private float b = 500.0f;
+        private const float freqCoef = 5;
 
         LibPdInstance puredataInstance;
         Camera user;
         GameObject target;
         Collider targetCol;
 
+        public Vector3 userToTargetVector;
+        private Ray pointedDirection;
+        public Vector2 userToTargetVerticalVector, userToTargetHorizontalVector, verticalPointedDirection, horizontalPointedDirection;
         private float distance;
-        private float verticalAngle;
-        private float horizontalAngle;
+        private float horizontalAngle, verticalAngle;
+
+        private float frequency;
 
         // Use this for initialization
         void Awake()
         {
             puredataInstance = GetComponent<LibPdInstance>();
-            Debug.Log(puredataInstance.patchName);
             user = Camera.main;
             target = GameObject.FindWithTag("Pickup");
             targetCol = target.GetComponent<Collider>();
@@ -29,17 +30,67 @@ namespace audio
                targetCol.ClosestPoint(user.transform.position),
                user.transform.position
            );
-            verticalAngle = 0;
-            horizontalAngle = 0;
-            
+            userToTargetVector = target.transform.position - user.transform.position;
+            pointedDirection = user.ScreenPointToRay(Input.mousePosition);
+            userToTargetVerticalVector = new Vector2(userToTargetVector.y - pointedDirection.direction.y, userToTargetVector.z - pointedDirection.direction.z);
+            userToTargetHorizontalVector = new Vector2(userToTargetVector.x - pointedDirection.direction.x, userToTargetVector.z - pointedDirection.direction.z);
+            verticalPointedDirection = new Vector2(pointedDirection.direction.y, pointedDirection.direction.z);
+            horizontalPointedDirection = new Vector2(pointedDirection.direction.x, pointedDirection.direction.z);
         }
 
         // Update is called once per frame
         void Update()
         {
-            horizontalAngle = Mathf.Acos(Vector2.Dot(new Vector2(user.transform.forward.x, user.transform.forward.z), 
-                                                    new Vector2(target.transform.forward.x, target.transform.forward.z)));
-            puredataInstance.SendFloat("freq", horizontalAngle*linearLink+b);
+            userToTargetVector = target.transform.position - user.transform.position;
+            userToTargetHorizontalVector.Set(userToTargetVector.x, userToTargetVector.z);
+            userToTargetVerticalVector.Set(userToTargetVector.y, userToTargetVector.z);
+            pointedDirection = user.ScreenPointToRay(Input.mousePosition);
+            verticalPointedDirection.Set(pointedDirection.direction.y, pointedDirection.direction.z);
+            horizontalPointedDirection.Set(pointedDirection.direction.x, pointedDirection.direction.z);
+
+            horizontalAngle = Vector2.SignedAngle(userToTargetHorizontalVector, horizontalPointedDirection);
+            if (horizontalAngle < 90 && horizontalAngle > -90)
+            {
+                if (horizontalAngle > 15)
+                {
+                    puredataInstance.SendFloat("left", 0);
+                    puredataInstance.SendFloat("right", 1);
+                }
+                else if (horizontalAngle < -15)
+                {
+                    puredataInstance.SendFloat("left", 1);
+                    puredataInstance.SendFloat("right", 0);
+                }
+                else
+                {
+                    puredataInstance.SendFloat("left", 1);
+                    puredataInstance.SendFloat("right", 1);
+                }
+            }
+            else
+            {
+                puredataInstance.SendFloat("left", 0);
+                puredataInstance.SendFloat("right", 0);
+            }
+
+            verticalAngle = Vector2.SignedAngle(userToTargetVerticalVector, verticalPointedDirection);
+            if (verticalAngle < 15 && verticalAngle > -15)
+                frequency = 200;
+            else if (verticalAngle > 15)
+                frequency = 100;
+            else frequency = 400;
+            //frequency = Mathf.Exp(Mathf.Log(1 / verticalAngle)) + 100;
+            //Debug.Log(userToTargetVerticalVector + " " + r.direction + " vAngle :" + verticalAngle + " frequence:" + freq);
+            //Debug.DrawRay(r.origin, r.direction * 10, Color.red);
+            //Debug.DrawLine(user.transform.position, target.transform.position, Color.green);
+            puredataInstance.SendFloat("freq", frequency);
+            Debug.Log(horizontalAngle+" "+verticalAngle+" "+frequency+" "+ userToTargetVector.sqrMagnitude);
+
+            /*distance = userToTargetVector.sqrMagnitude;
+            if (distance < 1)
+                puredataInstance.SendFloat("gain", 50);
+            else puredataInstance.SendFloat("gain", 20);*/
+
         }
     }
 }
